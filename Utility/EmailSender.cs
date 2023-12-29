a446b8633e7a97cc83a656b2033c21b6f348bdf0
hiro12a <ythom87@gmail.com>
2023-12-28 21:41:51 -0500

    Emailsender

---
+++ Utility/EmailSender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Utility
+{
+    public class EmailSender : IEmailSender 
+    {
+        public Task SendEmailAsync(string email, string subject, string body)
+        {
+            var mail = "thomtest965@gmail.com"; // Email that will be used to send 
+            var pw = "nvae fcwl qqob ofgw";
+
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, pw)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(
+                    from: mail,
+                    to: email,
+                    subject, 
+                    body
+                    ));
+        }
+    }
+}
