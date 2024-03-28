8ade259cf3c28923ca050bc1fde737fbab924e77
hiro12a <ythom87@gmail.com>
2024-03-28 10:21:45 -0400

    Added Images

--- obj/Debug/net8.0/PortfolioProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortfolioProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b717086152d5f2f9d741a5c0557b862ebd57d07")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
